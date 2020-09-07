d1b360f5db66bbfaaae9b887637b03c0036f6d15
CasaRol <alexander-rol@hotmail.com>
2020-09-07 17:42:33 +0200

    Tried to add function to save to json file but didn't work
    ToDo: Add json saving for entering new ToDo's and fetch them later and add directyly to the list.

--- ToDo.js
+++ ToDo.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 function saveToDo(task) {
     'use strict'
     
-    
-    
     let data = JSON.stringify(task);
     
     fs.writeFileSync('file.json', data, finished);
