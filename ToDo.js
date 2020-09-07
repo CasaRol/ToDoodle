11c179822dfade58f2ba9520ad1d119103afd258
CasaRol <alexander-rol@hotmail.com>
2020-09-07 17:09:17 +0200

    small changes to the index along with a few scrips for filling the lists display

---
+++ ToDo.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+function saveToDo(task) {
+    'use strict'
+    
+    
+    
+    let data = JSON.stringify(task);
+    
+    fs.writeFileSync('file.json', data, finished);
+}
